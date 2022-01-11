64167254036b2dc0b2b87dc570829299cc0fa85f
LEDA-GAO <ledagao@hotmail.com>
2022-01-10 23:59:20 -0500

    Update leetP1.cpp

--- leetP1.cpp
+++ leetP1.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <unordered_map> 
+#include <algorithm>
 using namespace std;
 class Solution 
 {
     public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> rind;
-        for(int i = 0;i<(nums.size()-1);i++){
-           for(int j =i+1;j<nums.size();j++){
-              if(nums[i]+nums[j]==target) 
-                   rind.insert(rind.end(),{i,j});
-                    return rind;                
+        int res;
+        unordered_map<int, int> umapt;
+        for (int i = 0; i < (nums.size() - 1); i++) {
+            res = target - nums[i];
+            if (umapt.count(res) == 0) {
+                umapt[nums[i]] = i;
+            }
+            else
+            {
+                rind.insert(rind.end(), { i,umapt.at(res) });
+            }
+        }
+ //This is the brute force method that has complexity O(n^2)    
+ //       for(int i = 0;i<(nums.size()-1);i++){
+ //          for(int j =i+1;j<nums.size();j++){
+ //             if(nums[i]+nums[j]==target) 
+ //                  rind.insert(rind.end(),{i,j});
+ //                   return rind;                
         
-          }
-       } 
+ //         }
+ //      } 
+        sort(rind.begin(), rind.end());
        return rind;
     }
 
@@ -35,4 +50,5 @@ int main(){
     vector<int> indexn;
     indexn  =   solution.twoSum(numbers,target);
     solution.print(indexn);
+
 }
