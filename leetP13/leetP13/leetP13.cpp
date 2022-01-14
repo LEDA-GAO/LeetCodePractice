841be20d395ab40818e025e4c0d275dea86875bb
LEDA-GAO <ledagao@hotmail.com>
2022-01-14 00:26:25 -0500

    Create leetP13.cpp

---
+++ leetP13/leetP13/leetP13.cpp
@@ -0,0 +1,42 @@
+// leetP13.cpp : 
+//
+
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+
+using namespace std;
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, short int> umap = { {'I',1}, {'V',5},
+    {'X',10},{'L',50},{'C',100},{'D',500},{'M',1000} };
+        int sum = 0;
+        int nexts = 0;
+        for (size_t i = 0, ilen = s.size()-1; i < ilen; i++) {
+            int currents = umap[s[i]];
+            nexts = umap[s[i + 1]];
+            if (currents >= nexts) {
+                sum += currents;
+
+            }
+            else {
+                sum -= currents;
+            }
+        }
+        sum += nexts;
+        return sum;
+
+    }
+};
+
+int main()
+{
+    string test = "MCMXCIV";
+    Solution solution;
+    cout << solution.romanToInt(test) << endl;
+}
+
+
